1fd53c5a02ae592adb8dbda80dae872d0c50369e
damianlopez126 <731765@student.dusd.net>
2022-12-19 13:48:35 -0800

    ergrtg
    ergergdfger

--- FlappyBirdLopezP1/Assets/Scripts/Bird.cs
+++ FlappyBirdLopezP1/Assets/Scripts/Bird.cs
@@ -4,38 +4,46 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-    public float upForce = 200f;
-
-    private bool isDead = false;
-    private Rigidbody2D rb2d;
-    private Animator anim;
-
-    // Start is called before the first frame update
+    public float upForce;                    
+    private bool isDead = false;            
+    private Animator anim;                   
+    private Rigidbody2D rb2d;                
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+      
         anim = GetComponent<Animator>();
+       
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+       
         if (isDead == false)
         {
+           
             if (Input.GetMouseButtonDown(0))
             {
-                rb2d.velocity = Vector2.zero;
-                rb2d.AddForce(new Vector2(0, upForce));
+               
                 anim.SetTrigger("Flap");
+               
+                rb2d.velocity = Vector2.zero;
+               
+                rb2d.AddForce(new Vector2(0, upForce));
             }
         }
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        isDead = true;
-        anim.SetTrigger("Die");
-
        
+        rb2d.velocity = Vector2.zero;
+        
+        isDead = true;
+        
+        anim.SetTrigger("Die");
+       
+        GameControl.instance.BirdDied();
     }
 }
 
